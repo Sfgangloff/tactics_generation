09abd4e58e4b166f3057ce25d63070da896343f0
Mirek Olšák <mirek@olsak.net>
2026-03-24 15:54:46 +0100

    make `VibeTactics` importable

---
+++ VibeTactics.lean
@@ -0,0 +1,3 @@
+import VibeTactics.decide_list_theory.plan_with_lsp.«decide_list_theory.milestone10»
+import VibeTactics.limit_auto.plan_with_lsp.milestone9
+import VibeTactics.nonzero.plan_with_lsp.«output.milestone10»
